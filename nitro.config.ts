8d5fd6cef689910a9440876588ec97d6581fc8e1
Sai Krishna Thati <thatisaikrishna39@gmail.com>
2026-08-14 14:05:06 +0530

    Fix CSP to allow eval for React

--- nitro.config.ts
+++ nitro.config.ts
@@ -4,4 +4,11 @@ export default defineNitroConfig({
   preset: "vercel",
   serveStatic: true,
   compatibilityDate: "2024-01-01",
+  routeRules: {
+    "/**": {
+      headers: {
+        "Content-Security-Policy": "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob:; connect-src 'self';",
+      },
+    },
+  },
 });
