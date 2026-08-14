9e770a2e97908f43e836077b0f3e44c6bde83c03
Sai Krishna Thati <thatisaikrishna39@gmail.com>
2026-08-14 13:25:13 +0530

    Update Nitro config with proper settings

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,3 +1,7 @@
-export default {
+import { defineNitroConfig } from "nitro/config";
+
+export default defineNitroConfig({
   preset: "vercel",
-};
+  serveStatic: true,
+  compatibilityDate: "2024-01-01",
+});
